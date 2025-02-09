d5bb74c1f7b70fad9d957357416e8fc68f7da03e
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-02-09 11:26:45 -0800

    Added loading spinner component

--- ProgrammingClass5.Angular/programmingclass5.angular.client/src/app/app.module.ts
+++ ProgrammingClass5.Angular/programmingclass5.angular.client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { ProductListComponent } from './products/list/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { CreateProductComponent } from './products/create/create-product.component';
 import { EditProductComponent } from './products/edit/edit-product.component';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     CreateProductComponent,
-    EditProductComponent
+    EditProductComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
--- ProgrammingClass5.Angular/programmingclass5.angular.client/src/app/products/list/product-list.component.ts
+++ ProgrammingClass5.Angular/programmingclass5.angular.client/src/app/products/list/product-list.component.ts
@@ -9,15 +9,23 @@ export class ProductListComponent implements OnInit {
   private readonly _productService: ProductService;
 
   public products: Product[] = [];
+  public isLoading: boolean = false;
 
   constructor(productService: ProductService) {
     this._productService = productService;
   }
 
   public ngOnInit(): void {
+    this.isLoading = true;
+
     this._productService.getAll()
       .subscribe(products => {
         this.products = products;
+        this.isLoading = false;
       });
+  }
+
+  public hideSpinner(): void {
+    this.isLoading = false;
   }
 }
